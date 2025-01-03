0aae7b7ca079f47da890f7d2501735870cdf34e1
gd2872 <gd2872@naver.com>
2025-01-03 16:32:48 +0900

    franchise package 추가

---
+++ src/main/java/com/haruon/groupware/franchise/mapper/FranchiseMapper.java
@@ -0,0 +1,8 @@
+package com.haruon.groupware.franchise.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FranchiseMapper {
+
+}
---
+++ src/main/java/com/haruon/groupware/franchise/service/FranchiseService.java
@@ -0,0 +1,10 @@
+package com.haruon.groupware.franchise.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class FranchiseService {
+
+}
