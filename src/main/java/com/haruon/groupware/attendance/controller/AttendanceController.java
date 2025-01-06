b54ebe04eb06c7873025229db04cef1fbd1e00de
eunseo <eunseokim43@gmail.com>
2025-01-06 16:25:37 +0900

    Revert "(은서) 근태관리 뷰 생성"
    This reverts commit e12a8e9204e366cee3ceed5144d86ab79486d4b9.

--- src/main/java/com/haruon/groupware/attendance/controller/AttendanceController.java
+++ src/main/java/com/haruon/groupware/attendance/controller/AttendanceController.java
@@ -1,20 +1,8 @@
 package com.haruon.groupware.attendance.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AttendanceController {
-	
-	// 개인 근태/연가/출장 관리 페이지
-	@GetMapping("/employee/attendance")
-	public String attendanceEmp() {
-		return "/attendance/attendanceEmp";
-	}
-	// 부서 근태/연가/출장 관리 페이지
-	@GetMapping("/employees/attendance")
-	public String attendanceDivision() {
-		return "/attendance/attendanceDivision";
-	}
 
 }
