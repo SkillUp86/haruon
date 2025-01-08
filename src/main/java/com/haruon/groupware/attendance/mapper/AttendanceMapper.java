3c58e3186bb28d1d169c85afc2ec87d7b3788424
eunseo <eunseokim43@gmail.com>
2025-01-08 17:49:44 +0900

    (은서) 근태 스케쥴링 진행중

--- src/main/java/com/haruon/groupware/attendance/mapper/AttendanceMapper.java
+++ src/main/java/com/haruon/groupware/attendance/mapper/AttendanceMapper.java
@@ -1,13 +1,25 @@
 package com.haruon.groupware.attendance.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.haruon.groupware.user.entity.Emp;
+import com.haruon.groupware.attendance.entity.Attendance;
 
 @Mapper
 public interface AttendanceMapper {
-
 	
+	// 특정 사원의 어제 근태 기록 가져오기
+	Attendance findAttendance(Attendance attendance);
+	// 특정 사원의 어제 연차/반차/출장 기록 가져오기 
+	List<Map<String, Object>> findYesterDayAttByEmpAndDay(Attendance attendance);
+	// 1. Insert 대상인지 update 대상인지 파악
+	// 1-1. 특정 일 & 직원의 근태상태, 연가유무 및 시작 ~ 종료시작, 출장유무 및 시작 ~ 종료시간 조회
+	// 1-2. 1. 행이 없다면, 특정 일 & 직원의 출장 및 연가 기록
+	
+	// 근태 업데이트 (수정대상 : 종료시간, 상태, 승인여부, 수정사유) (필요 조건값 : empNo. startDate)
+	Integer updateAttendance(Attendance attendance);
+	// 근태 생성 (컬럼 : emp_no, start_time, end_time, state) 
+	Integer insertAttendance(Attendance attendance);
 }
--- src/main/java/com/haruon/groupware/attendance/service/AttendanceService.java
+++ src/main/java/com/haruon/groupware/attendance/service/AttendanceService.java
@@ -1,15 +1,23 @@
 package com.haruon.groupware.attendance.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.haruon.groupware.attendance.entity.Attendance;
 import com.haruon.groupware.attendance.mapper.AttendanceMapper;
 import com.haruon.groupware.user.entity.Emp;
+import com.haruon.groupware.user.mapper.EmpMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,17 +25,75 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Transactional
 public class AttendanceService {
-	@Autowired AttendanceMapper attendanceMapper;
+	@Autowired private AttendanceMapper attendanceMapper;
+	@Autowired private EmpMapper empMapper;
 	
-
+	// 하루 전
+	private LocalDate yesterdayLD
+					= LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
+	private String yesterday = yesterdayLD.toString();
+	
+	// 하루 전이 속한 달의 1일 
+	private String listTargetStart = LocalDate.of(yesterdayLD.getYear(), yesterdayLD.getMonthValue(), 1).toString();
+	
 	
 	@Scheduled(cron = "00 00 00 * * *")
-	public void schedulePreviousDayAttState() {
-		// 1) 퇴사일자가 null인 1모든 직원 리스트 가져오기 
+	public void schedulePreviousDayAttState() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// 1) 퇴사일자가 null인 모든 직원 리스트 가져오기 - 스케쥴링 대상
+		List<Emp> empList = empMapper.findAllEmp();
 		
-		// 2) 전날 + 특정 직원의 
-		// 2) attedance DB 필터링 : emp_no와 start_time을 기준으로 state가 null인 경우 : insert대상 // null이 아니면 update대상 
+		Attendance yesterDayAtt = new Attendance(); // 특정 직원의 어제자 근태 데이터 행
+		Attendance newAtt = new Attendance();	// 갱신된 attendance 인스턴스
+		List<Map<String, Object>> findVacationAtt = new ArrayList<>(); // 어제의 사원의 연차/출장 기록
 		
+		// 2) (전날 + 특정 직원)으로 attendance테이블에 행이 있는지 판단 - 있으면 해당행을 update 하고, 없으면 해당행을 insert 하고
+		loopOut :
+		for(Emp emp : empList) {
+			yesterDayAtt.setEmpno(emp.getEmpNo());
+			yesterDayAtt.setStart_time(yesterday);
+			
+			
+			yesterDayAtt = attendanceMapper.findAttendance(yesterDayAtt);
+	        findVacationAtt = attendanceMapper.findYesterDayAttByEmpAndDay(yesterDayAtt);
+			
+			if(yesterDayAtt != null) {
+				
+		        Date workStartTime = dateFormat.parse(yesterDayAtt.getStart_time());
+		        Date workEndTime = dateFormat.parse(yesterDayAtt.getEnd_time());
+
+		        // 근무시간(workHour) 확인
+		        Integer workHour = (int)((workStartTime.getTime() - workEndTime.getTime()) / 1000 / 60 / 60 );
+		        
+		        // 근무시간 별 근태 기록 업데이트 + 생성
+		        if(workHour < 5) {
+		        	
+		        } else if (workHour < 9) {
+		        	
+		        } else {
+		        	
+		        }
+		        
+			} else {
+				
+				// 연차인지 확인
+				for(Map<String, Object> sch : findVacationAtt) {
+		        	if(sch.get("schType").equals("연차")) {
+		        		newAtt.setEmpno(emp.getEmpNo());
+		        		newAtt.setStart_time(yesterday + " 00:00:00");
+		        		newAtt.setEnd_time(yesterday + " 23:59:59");
+		        		newAtt.setState("연차");
+		        		continue loopOut;
+		        	}
+	        	}
+			}
+			
+			
+		}
+		
+		
+		// 3) 결과 확인
+		 	
 	}
 }
 
