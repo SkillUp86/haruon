2cf18a6b4f643272e6f85e437611178b702ea795
eunseo <eunseokim43@gmail.com>
2025-01-13 12:07:34 +0900

    (은서) 로그인 기능 변경으로 인한, Attendance부분 파라미터 전달 방식 변경(Session -> Authentication)

--- src/main/java/com/haruon/groupware/common/controller/HomeController.java
+++ src/main/java/com/haruon/groupware/common/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.haruon.groupware.common.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.haruon.groupware.auth.CustomUserDetails;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Authentication authentication, Model model) {
+		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+		model.addAttribute("empNo", userDetails.getEmpNo());
+		
 		return "home";
 	}
 	
