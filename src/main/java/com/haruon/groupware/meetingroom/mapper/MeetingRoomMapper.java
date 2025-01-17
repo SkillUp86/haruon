9f79473b79587b96a332a3f7b5954673ceec3913
najungwoo <najwoo0318@naver.com>
2025-01-17 17:39:46 +0900

    회의실 구현중

--- src/main/java/com/haruon/groupware/meetingroom/mapper/MeetingRoomMapper.java
+++ src/main/java/com/haruon/groupware/meetingroom/mapper/MeetingRoomMapper.java
@@ -1,8 +1,14 @@
 package com.haruon.groupware.meetingroom.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.haruon.groupware.meetingroom.entity.MeetingRoom;
 
 @Mapper 
 public interface MeetingRoomMapper {
+	
+	List<MeetingRoom>meetingroomList();
 
 }
