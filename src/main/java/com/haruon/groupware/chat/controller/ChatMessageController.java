9bf2c0d82a8cc040e403b9b96c132b0080e68666
eunseo <eunseokim43@gmail.com>
2025-01-21 15:53:28 +0900

    (은서) messageController 주석처리

--- src/main/java/com/haruon/groupware/chat/controller/ChatMessageController.java
+++ src/main/java/com/haruon/groupware/chat/controller/ChatMessageController.java
@@ -10,17 +10,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class ChatMessageController {
-	
-	private final SimpMessageSendingOperations messagingTemplate;
-	
-	public ChatMessageController(SimpMessageSendingOperations messagingTemplate) {
-		this.messagingTemplate = messagingTemplate;
-	}
-
-	// 채팅 메세지 전달 
-	@MessageMapping("/chat/message")
-	public void message(Chat chat) {
-		messagingTemplate.convertAndSend("/sub/chat/room" + chat.getRoomId(), chat);
-	}
+//	
+//	private final SimpMessageSendingOperations messagingTemplate;
+//	
+//	public ChatMessageController(SimpMessageSendingOperations messagingTemplate) {
+//		this.messagingTemplate = messagingTemplate;
+//	}
+//
+//	// 채팅 메세지 전달 
+//	@MessageMapping("/chat/message")
+//	public void message(Chat chat) {
+//		messagingTemplate.convertAndSend("/sub/chat/room" + chat.getRoomId(), chat);
+//	}
 	
 }
