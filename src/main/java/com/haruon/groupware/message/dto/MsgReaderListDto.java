322189c844da802763abf472a175fd9e203d1923
eunseo <eunseokim43@gmail.com>
2025-01-24 10:23:50 +0900

    (은서) 상태코드 descript -> common_code 사용 쿼리 정정 /  헤더 메세지 요약 정보 컬럼 추가

--- src/main/java/com/haruon/groupware/message/dto/MsgReaderListDto.java
+++ src/main/java/com/haruon/groupware/message/dto/MsgReaderListDto.java
@@ -8,4 +8,6 @@ public class MsgReaderListDto {
 	private String senderName;	// 발신자 이름
 	private String senderProfileName;	// 발신자 프로필 파일명
 	private String senderProfileExt;	// 발신자 프로필 확장자명
+	private Integer msgNo;
+	private String sendDate;
 }
